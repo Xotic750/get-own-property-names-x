bbd26992c2e155593259a6cf0d2cbaf1509e4de0
Graham Fairweather <xotic750@gmail.com>
2019-07-16 22:46:34 +0200

    Terraformed
    Auto build

--- __tests__/get-own-property-names-x.test.js
+++ __tests__/get-own-property-names-x.test.js
@@ -1,28 +1,5 @@
 let getOwnPropertyNames;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  getOwnPropertyNames = require('../../index.js');
-} else {
-  getOwnPropertyNames = returnExports;
-}
-
 const win = typeof window !== 'undefined' && window;
 const doc = typeof document !== 'undefined' && document;
 const ifBrowserIt = win && doc ? it : xit;
