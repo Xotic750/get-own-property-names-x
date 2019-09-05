2c20df8a2eaf74bb22f6354062e6cb359a156452
Graham Fairweather <xotic750@gmail.com>
2019-09-05 02:14:17 +0200

    Maintenance

--- dist/get-own-property-names-x.js
+++ dist/get-own-property-names-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T22:46:12.546Z",
+  "date": "2019-09-05T00:14:09.947Z",
   "describe": "",
   "description": "Creates an array of all properties (enumerable or not) found directly upon a given object.",
   "file": "get-own-property-names-x.js",
